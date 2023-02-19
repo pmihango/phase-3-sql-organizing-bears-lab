fdbbdb166f66ea52aa9c86dca88d8ea016cab488
pmihango <pmihango@gmail.com>
2023-02-19 19:17:39 +0300

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,18 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Rowdy', 2, 'M', 'Black', 'wild', '1' );
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Tabitha', 4, 'F', 'Grey', 'soft', '1' );
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Sergeant Brown', 5, 'M', 'Beige', 'crazy', '1');
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Melissa', 2, 'F', 'Black', 'temper', '0');
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Grinch', 6, 'M', 'Grey', 'slow', '1' );
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Wendy', 3, 'F', 'Brown', 'cool', '1');
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('null', 7, 'F', 'Black', 'wild', '1' );
+
+
+
+
+
+
+
+
+
+
+
